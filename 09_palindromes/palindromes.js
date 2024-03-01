8f1eb1bbe7764c3d8d493fb81f3f1423b5b1c6ef
Eleuthur <105822177+Eleuthur@users.noreply.github.com>
2024-03-01 13:56:37 +0000

    basic function done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const origString = string.split('');
+    const revString = origString;
+    revString.reverse();
+    let origMarker = 0;
+    let revMarker = revString.length - 1;
+    for (let i = 0; i = string.length; i++) {
+        if (origString[origMarker] === revString[revMarker]) {
+            i++;
+            origMarker++;
+            revMarker--;
+        } else if (origString[origMarker].match(/[a-z]/i)) {
+            origMarker++;
+        } else if (revString[revMarker].match(/[a-z]/i)) {
+            revMarker--;
+        } else return(false);
+        return(true);
+    }
 };
 
 // Do not edit below this line
